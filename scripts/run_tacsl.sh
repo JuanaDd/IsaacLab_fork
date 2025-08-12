98c0571b04f44c1f10440904f9e68007c1fbf7db
JuanaDu <yvetted@nvidia.com>
2025-08-12 19:00:24 +0800

    minor updates on arguments

--- scripts/run_tacsl.sh
+++ scripts/run_tacsl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./tacsl/tacsl_example.py --enable_cameras --indenter nut --num_envs 8 --save_viz True
+python ./tacsl/tacsl_example.py --enable_cameras --indenter nut --num_envs 256 --use_tactile_rgb --use_tactile_ff --play_reset
